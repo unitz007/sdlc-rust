4ce185f89092beefd4dda84389edf02499e5ec58
charlesDinneya <dinneyacharles007@gmail.com>
2023-05-14 16:11:24 +0200

    first commit to sdlc-rust

---
+++ src/lib.rs
@@ -0,0 +1,28 @@
+pub mod model;
+
+use std::fs::*;
+use std::io::Read;
+
+
+pub fn read_json(path: &str) -> String {
+
+    let mut file = File::open(path.to_string() + "sdlc.json")
+        .expect("configuration file not found");
+
+    let mut json_content = String::new();
+
+    file.read_to_string(&mut json_content).expect("error reading configurations");
+
+    json_content
+}
+
+
+pub fn get_task<'a>(arg: &'a str, task: &'a model::Tasks) -> &'a str {
+
+    match arg {
+        "run" => task.run.as_str(),
+        "test" => task.test.as_str(),
+        "build" => task.build.as_str(),
+        _ => panic!("Invalid sdlc command")
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,67 @@
+mod cli;
+
+use std::fs::*;
+use std::process::{Command, Stdio};
+use std::io::{Error, BufReader, ErrorKind, BufRead};
+use std::env;
+use std::borrow::Borrow;
+use sdlc::model::{Build};
+
+fn main() -> Result<(), Error> {
+
+    let program = env::current_exe().unwrap();
+
+    let program_name = program.file_name().unwrap();
+
+    let program_full_path = env::current_exe()
+        .unwrap()
+        .as_path()
+        .to_str()
+        .unwrap()
+        .to_string();
+
+    let program_path = program_full_path.split_at(program_full_path.len() - program_name.len()).0;
+
+    let contents = sdlc::read_json(program_path.borrow());
+
+    let args: Vec<String> = env::args().skip(1).collect();
+
+    if args.is_empty() {
+        panic!("Please provide an argument");
+    }
+
+    let current_directory = std::env::current_dir().expect("error accessing directory");
+
+    let build: Build = serde_json::from_str(&contents).unwrap();
+
+    let mut program = "";
+    let mut com = "";
+
+    for command in build.builds.iter() {
+        let build_file = &command.build_file;
+
+        for f in read_dir(&current_directory).unwrap() {
+            let file = f.unwrap().file_name().into_string().unwrap();
+            if file.eq(build_file) {
+                program = &command.tasks.program;
+                let arg = &args[0];
+                com = sdlc::get_task(arg, &command.tasks);
+            }
+        }
+    }
+
+    let output = Command::new(program)
+        .args(&[com.to_string()])
+        .stdout(Stdio::piped())
+        .spawn()?
+        .stdout
+        .ok_or_else(|| Error::new(ErrorKind::Other, "Could not capture standard output."))?;
+
+    let reader = BufReader::new(output);
+
+    reader.lines()
+        .filter_map(|line| line.ok())
+        .for_each(|line| println!("{}", line));
+
+    Ok(())
+}
